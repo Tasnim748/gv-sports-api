6e0f6eb451accc6438a2269bc57104114579e75f
Tasnim Jakir <tasnim748@gmail.com>
2022-07-21 15:40:52 +0600

    2nd deployment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 const bookingRouter = require('./routers/bookingRouter')
 
 const app = express();
 
 // app config
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 
